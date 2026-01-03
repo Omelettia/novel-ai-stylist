76aca1345a031a32e1ba073362de3bf691c8b342
Omelettia <taikhoanguyen@tutanota.com>
2026-01-03 18:00:38 +0700

    fix bug

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 const isLocal = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost";
 
-export const API = isLocal ? "http://127.0.0.1:8000" : ""; 
+export const API_URL = isLocal ? "http://127.0.0.1:8000" : ""; 
 
 export const MAX_HISTORY = 50;
